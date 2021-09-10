2721793c13089a0e01a596d9720d907b91ff6ab4
carlosa añorve <carlosaarivera23@gmail.com>
2021-09-10 03:18:56 -0500

    was updated for get the key from environ variables when no find in the parameters file

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,11 +1,21 @@
 import configparser
+import os
 
 config = configparser.ConfigParser()
 config.read('app/parameters')
 
 ACCESS_KEY_FIXER = config['DEFAULT']['ACCESS_KEY_FIXER']
 TOKEN_BMX = config['DEFAULT']['TOKEN_BMX']
+if not ACCESS_KEY_FIXER:
+    ACCESS_KEY_FIXER = os.environ.get('ACCESS_KEY_FIXER')
+if not TOKEN_BMX:
+    TOKEN_BMX = os.environ.get("TOKEN_BMX")
+
 TOKEN_TEST = config['DEFAULT']['TOKEN_TEST']
 TIMES = int(config['DEFAULT']['TIMES'])
 MINUTES = int(config['DEFAULT']['MINUTES'])
 SECONDS = int(config['DEFAULT']['SECONDS'])
+
+
+print("TOKEN_BMX", TOKEN_BMX, bool(TOKEN_BMX))
+print("ACCESS_KEY_FIXER", ACCESS_KEY_FIXER, bool(ACCESS_KEY_FIXER))
